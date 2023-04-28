49b0ca1d7383190534b6e158cf182eed4a2ce6aa
Laurent Pugin <lxpugin@gmail.com>
2023-04-28 14:55:19 +0200

    Reset options in VerovioApp

--- dist/verovio-app.js
+++ dist/verovio-app.js
@@ -2,6 +2,7 @@ import { App } from './app.js';
 export class VerovioApp extends App {
     constructor(div, options) {
         options.enableEditor = false;
+        options.appReset = true;
         super(div, options);
     }
 }
--- ts/verovio-app.ts
+++ ts/verovio-app.ts
@@ -4,6 +4,7 @@ import { App } from './app.js';
 export class VerovioApp extends App {
     constructor(div: HTMLDivElement, options: App.Options) {
         options.enableEditor = false;
+        options.appReset = true;
         super(div, options);
     }
 }
